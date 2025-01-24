b39fb6e8f7c7f02e874972445712568b0f6f6440
Slawomir Dyk <slawomir.dyk@astrosat.net>
2025-01-24 11:35:50 +0000

    bulid an admin dashboard components

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,45 @@
-export function AdminDashboard() {
-  return <div>Dashboard</div>;
-}
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card";
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+const AdminDashboard = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle="Test Sales"
+        body="This is body description"
+      />
+    </div>
+  );
+};
+
+export default AdminDashboard;
+
+export const DashboardCard = ({
+  title,
+  subtitle,
+  body,
+}: DashboardCardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
+};
